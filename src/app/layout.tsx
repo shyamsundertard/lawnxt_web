49e04f07acca2ba73b83310a13e297bbd1d51e63
Shyamsunder Tard <iamshyamsundertard@gmail.com>
2025-05-19 15:01:48 +0530

    Initial commit from Create Next App

---
+++ src/app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "LawNXT",
+  description: "You law partner...",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+      <main className="lg:mx-6 mx-4">
+        {children}
+      </main>
+      </body>
+    </html>
+  );
+}
